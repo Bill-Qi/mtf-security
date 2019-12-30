f606a50953f02589b82c7f681bc645b328545fdb
Bill-Qi <57887876+Bill-Qi@users.noreply.github.com>
2019-12-30 17:38:04 +0800

    5-8

--- mtf-security-core/src/main/java/com/mtf/security/core/social/weixin/config/WeixinAutoConfiguration.java
+++ mtf-security-core/src/main/java/com/mtf/security/core/social/weixin/config/WeixinAutoConfiguration.java
@@ -1,13 +1,17 @@
 package com.mtf.security.core.social.weixin.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.social.SocialAutoConfigurerAdapter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.social.connect.ConnectionFactory;
+import org.springframework.web.servlet.View;
 
 import com.mtf.security.core.properties.SecurityProperties;
 import com.mtf.security.core.properties.WeixinProperties;
+import com.mtf.security.core.social.MtfConnectView;
 import com.mtf.security.core.social.weixin.connect.WeixinConnectionFactory;
 
 /** 
@@ -35,5 +39,13 @@ public class WeixinAutoConfiguration extends SocialAutoConfigurerAdapter {
 		return new WeixinConnectionFactory(weixinConfig.getProviderId(), weixinConfig.getAppId(),
 				weixinConfig.getAppSecret());
 	}
+	
+	
+	@Bean({"connect/weixinConnect", "connect/weixinConnected"})
+	@ConditionalOnMissingBean(name = "weixinConnectedView")
+	public View weixinConnectedView() {
+		return new MtfConnectView();
+	}
+	
 
 }
